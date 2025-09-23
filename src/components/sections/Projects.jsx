d5ad879be02405cc4aaec7942ec22b7c7daf007c
Panupong Chaichun <panuza1@users.noreply.github.com>
2025-09-23 12:37:46 +0700

    test

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -10,9 +10,9 @@ export const Projects = () => {
                 {/*EDIT HERE*/}
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> Calculate_entropy_and_weight_avg_entropy</h3>
+                    <h3 className="text-xl font-bold mb-2"> test test</h3>
                     <p className="text-gray-400 mb-4">
-                        for calculate entropy equation and weight average entropy
+                        test test
                     </p>
                     <div>
                         {["Python"].map((tech, key) => (
